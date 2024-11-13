3cd6cdad5d92fe5a903ffb1d3b21dfb4fb151567
Jay Araña <jadiearana@gmail.com>
2024-11-13 09:38:42 +0800

    Add files via upload

---
+++ LABORATORYEXERCISE.java
@@ -0,0 +1,27 @@
+
+package laboratoryexercise;
+
+
+public class LABORATORYEXERCISE {
+
+    public static void main(String[] args) {
+        byte iNumber = 3;
+
+        if (iNumber >= 0) {
+            System.out.println("First string");
+
+            System.out.println("Second string");
+
+        } 
+        
+        else {
+        }
+
+            System.out.println("Third string");
+   
+    
+    
+        }
+    }
+
+
